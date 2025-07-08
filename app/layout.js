c306bc1bf51958c9272cfc9c6ad0e554cd1dc592
rana <rana.buddy@gmail.com>
2025-07-08 21:32:40 +0600

    feat:xioncart sh creation

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,27 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Cash App",
   description: "Cash App",
+  openGraph: {
+    title: "Cash App",
+    description: "Cash App",
+    url: "https://cashappfront.vercel.app", // your real page URL
+    siteName: "cashapp",
+    images: [
+      {
+        url: "https://newsite-livid.vercel.app/cashmeta.jpg", // ✅ Your image URL
+        width: 1200,
+        height: 630,
+        alt: "CashMeta Image",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "Cash App",
+    title: "Cash App",
+    description: "Cash App",
+    images: ["https://newsite-livid.vercel.app/cashmeta.jpg"],
+  },
 };
 
 export default function RootLayout({ children }) {
